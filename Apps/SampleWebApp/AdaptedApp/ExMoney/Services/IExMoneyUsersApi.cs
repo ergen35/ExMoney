985f2ce447d6c4ef64b1dc0de47272ff5bc511d2
wassi <Adjadowassi5@gmail.com>
2023-05-17 18:34:26 +0100

    Updated Dashboard

--- Apps/SampleWebApp/AdaptedApp/ExMoney/Services/IExMoneyUsersApi.cs
+++ Apps/SampleWebApp/AdaptedApp/ExMoney/Services/IExMoneyUsersApi.cs
@@ -1,9 +1,12 @@
 using System;
+using ExMoney.SharedLibs;
+using Refit;
 
 namespace ExMoney.Services
 {
-    public class IExMoneyUsersApi
+    public interface IExMoneyUsersApi
     {
-        
+        [Get("/api/v1/users/{userId}")]
+        public Task<IApiResponse<User>> GteUserById(string userId);
     }
 }
